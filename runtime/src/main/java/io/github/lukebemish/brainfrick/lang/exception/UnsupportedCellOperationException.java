5fd609b6fda977fa03be20bc6b8a2478e21a7b39
Luke Bemish <lukebemish@gmail.com>
2022-08-16 20:51:01 -0600

    Document UnsupportedCellOperationException

--- runtime/src/main/java/io/github/lukebemish/brainfrick/lang/exception/UnsupportedCellOperationException.java
+++ runtime/src/main/java/io/github/lukebemish/brainfrick/lang/exception/UnsupportedCellOperationException.java
@@ -1,6 +1,14 @@
 package io.github.lukebemish.brainfrick.lang.exception;
 
+/**
+ * An exception thrown to indicate that a given brainfrick operation (such as incrementation or decrementing) cannot be
+ * applied to the given object.
+ */
 public class UnsupportedCellOperationException extends UnsupportedOperationException {
+    /**
+     * Creates a new {@link UnsupportedCellOperationException} with the given message
+     * @param msg The detail message.
+     */
     public UnsupportedCellOperationException(String msg) {
         super(msg);
     }
